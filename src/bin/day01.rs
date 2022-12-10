1659f5271f94e1c0e05fc4307d8b479377cf8c2d
Ruben Arts <rarts@smart-robotics.nl>
2022-12-10 19:55:07 +0100

    day1

---
+++ src/bin/day01.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // Read input file
+    let input = std::fs::read_to_string("inputs/day01.txt").unwrap();
+
+    let mut elf_list = Vec::new();
+    let mut current_sum = 0;
+    let mut lines = input.lines();
+    while let Some(line) = lines.next() {
+        if line.is_empty() {
+            elf_list.push(current_sum);
+            current_sum = 0;
+        } else {
+            current_sum += line.parse::<i32>().unwrap();
+        }
+    }
+    elf_list.sort();
+    elf_list.reverse();
+
+    println!("Solution 01: {}", elf_list[0]);
+    println!("Solution 02: {}", elf_list[0] + elf_list[1] + elf_list[2]);
+}
